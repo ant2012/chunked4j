7a5f9cb621200d974e7ff1f661b4f587fd8a860a
Andrey Murav'yov <ant@nicotech.ru>
2017-11-21 13:47:33 +0300

    reader logger

--- src/main/java/ru/ant/chunked4j/ChunkStreamReader.java
+++ src/main/java/ru/ant/chunked4j/ChunkStreamReader.java
@@ -1,9 +1,13 @@
 package ru.ant.chunked4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.Serializable;
 
 public abstract class ChunkStreamReader<R extends Serializable> implements Runnable {
     protected final ChunkInputStream stream;
+    protected final Logger log = LogManager.getLogger();
 
     public ChunkStreamReader(ChunkInputStream stream) {
         this.stream = stream;
